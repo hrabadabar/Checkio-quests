d33d6c9424261b5713613989e41a6daa0619e511
hrabadabar <39590668+hrabadabar@users.noreply.github.com>
2022-05-06 12:50:02 +0100

    Update Bigger Together.py

--- Bigger Together.py
+++ Bigger Together.py
@@ -24,13 +24,15 @@ def bigger_together(ints: List[int]) -> int:
     """
     
     max_length = max(len(str(x)) for x in ints)
-    # if it is any integer with maximum length - if maximum lenght is 2, 
-    # divide it by 10, lenght 3 - divided by 100 etc
+    # find the largest integer first(the one that has the most digits if there is one)
+    # if the integer has two digits, divide it by 10.
+    # if the integer has 3 digits, divide it by 100
+    # this way if you have 42 and 413, 413 becomes 4.13 and the concatenated number becomes 42413
     # if it is an integer of length smaller than maxumum length,
     # turn it into a float such that the digits to the right and the digits
-    # to the  left of the decimal point are equal - for example 43 becomes 43.43
-    # This way if you have 423 and 42 for example, 42 will go in front of 423
-    # and you will have 42423 maximum nu8mber and not 42342
+    # to the left of the decimal point are equal - for example 43 becomes 43.43
+    # This way if you have 423 and 42 for example, 42 wil become 42.42 and 423 - 4.23
+    # So we will have 42423 maximum number and not 42342
     maximum = sorted(ints,reverse = True, key = lambda x: float(str(x)+'.'+str(x)) if len(str(x)) < max_length else x/(10**(len(str(x))-1)))
     # The same as the above to get the minimum
     minimum = sorted(ints, key = lambda x: float(str(x)+'.'+str(x)) if len(str(x)) < max_length else x/(10**(len(str(x))-1)))
